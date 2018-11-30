7318636677f309d96f1e7e4c67c65889e16d8ee5
BinaryWiz <Ultimi450@gmail.com>
2018-11-29 23:50:44 -0500

    Removed unnecessary folders and files. Positioned GUI in ideal spot. Changed to dark theme to better fit Amazon.

--- retailers.js
+++
@@ -1,9 +0,0 @@
-var internal_check = ""
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        console.log("GOT THIS FAR");
-        if(request.status == "Edit" && internal_check != "Once") {
-            internal_check = "Once";
-        }
-    }
-);
