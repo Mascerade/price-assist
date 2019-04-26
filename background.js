b1a8982ab8f6dc8ba4f641a56af3974035458e5e
BinaryWiz <Ultimi450@gmail.com>
2019-04-26 09:07:57 -0400

    changed so that there is retailer variable for when the retailer is not amazon

--- background.js
+++ background.js
@@ -1,15 +1,17 @@
 var send = true; // Checks if we should send a message to display the results
+let retailer;
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     // Only runs when loading the tab is complete
     if (changeInfo.status == "complete") {
         // Only executes the rest of the code if the tab is on an Amazon product page
         if (tab.url.includes("www.amazon.com") && tab.url.includes("dp")){
+            retailer = "Amazon";
             // Sends a message to the content_script saying to check if the category is electronics
             chrome.tabs.sendMessage(tab.id, {status:"Check Electronics"}, function(response) {
                 // If the tab is on Electronics, content_script sends response to get data from server
                 if(response.status == "Get data") {
                     const http = new XMLHttpRequest(); // Defines the Http request
-                    let url = "http://www.timeless-apps.com/api/query?link=" + tab.url + "&amazon_price=" + response.price + "&item_model=" + response.item_model;
+                    let url = "http://www.timeless-apps.com/api/query?retailer=" + retailer + "&price=" + response.price + "&item_model=" + response.item_model;
                     http.open("GET", url); // Sets the request to a GET request
                     http.send(); // Sends the request to the server
                     http.onreadystatechange=(e)=> { // If something gets sent back from the server
