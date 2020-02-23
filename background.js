df2539a5ebab3c5f92f005c9534a4aed50408862
BinaryWiz <Ultimi450@gmail.com>
2020-02-22 19:11:57 -0500

    initial refactor. everything BASICALLY working

--- background.js
+++ background.js
@@ -1,48 +1,46 @@
 var send = true; // Checks if we should send a message to display the results
-let retailer;
-chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    // Only runs when loading the tab is complete
-    if (changeInfo.status == "complete") {
-        // Only executes the rest of the code if the tab is on an Amazon product page
-        if (tab.url.includes("www.amazon.com") && (tab.url.includes("dp") || tab.url.includes("gp"))){
-            retailer = "Amazon";
-            // Sends a message to the content_script saying to check if the category is electronics
-            chrome.tabs.sendMessage(tab.id, {status:"Check Electronics"}, function(response) {
-                // If the tab is on Electronics, content_script sends response to get data from server
-                if(response.status == "Get data") {
-                    const http = new XMLHttpRequest(); // Defines the Http request
-                    let localserver = "10.0.0.191:5000"
-                    let timelessServer = "timeless-apps.com"
-                    let url = "http://" + localserver + "/api/query?retailer=" + retailer +
-                    "&price=" + response.price + "&item_model=" + response.item_model + "&title=" + response.title + "&return_type=gui";
-                    // let url = "http://localhost:5000/api/query?retailer=" + retailer + "&price=" + response.price + "&item_model=" + response.item_model + "&return_type=gui";
-                    http.open("GET", url); // Sets the request to a GET request
-                    http.send(); // Sends the request to the server
-                    http.onreadystatechange=(e)=> { // If something gets sent back from the server
-                        // Converts the string coming from the server to an actual JSON
-                        JSON.stringify(JSON5.parse(http.responseText));
+let portFromCS;
+var retailer = "";
 
-                        // If there was an error getting the item model, don't display anything to the user
-                        if(http.responseText.includes("Error")){
-                            send = false;
-                        }
+chrome.runtime.onConnect.addListener(connected)
 
-                        // Send message only if there was actual info in server response
-                        if(send == true) {
-                            // Sends the information from the server to the content_script to display the information
-                            chrome.tabs.sendMessage(tab.id, {
-                                status: "Display",
-                                data: http.responseText
-                            }, function (response) {
-                            });
-                        }
-                    }
-                }
-                send = true;
-            });
+
+function connected(p) {
+    portFromCS = p;
+    portFromCS.onMessage.addListener(function(message) {
+        if (message.message == "put retailer") {
+            retailer = message.retailer;
+            console.log(retailer);
+            portFromCS.postMessage({message: "get info"});
         }
-    }
-});
+
+        if (message.message == "send request") {
+            const http = new XMLHttpRequest();
+            const localserver = "localhost:5000";
+            const timlessServer = "timeless-apps.com"
+            const url = "http://" + localserver + "/price-assist/api/network-scrapers" +
+            "?retailer=" + retailer + "&item_model=" + message.item_model + "&title=" +
+            message.title + "&return_type=gui";
+            http.open("GET", url);
+            http.send();
+            http.onload = (e) => {
+                console.log("DONE?")
+                data = JSON5.parse(http.responseText);
+                if (http.responseText.includes("Error")) {
+                    send = false;
+                }
+
+                else {
+                    console.log(typeof(http.responseText));
+                    portFromCS.postMessage({message: "add gui", iframe: data.iframe, head: data.head, body: data.body})
+                }
+            }
+        }
+    });
+}
+
+
+
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     // Sends a message to the content_script saying to change the visibility of the gui
--- content_script.js
+++ content_script.js
@@ -1,143 +1,117 @@
-var internal_check = ""; // This is to make sure that we only will display once
+var internal_check = 0; // This is to make sure that we only will display once
 var internal_display_count = 0; // Makes sure that only 1 gui will every be shown at a time
 var iframe; // Makes the global iframe variable
 var iframe_wrapper; // Makes the global iframe_wrapper
 
-// Only executes when we receive a message from background_script
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-        if(request.status === "Remove") {
 
-            // Checks if the iframe and iframe_wrapper has been made
-            if(typeof iframe_wrapper != undefined || typeof iframe_wrapper != null) {
-                // (FOR DEBUGGING) console.log(iframe_wrapper.style.visibility);
+let port = chrome.runtime.connect({name:"cs-port"});
 
-                // If the visibility of iframe_wrapper is already hidden, make it visible
-                if (iframe_wrapper.style.visibility === "hidden") {
-                    iframe_wrapper.style.setProperty("visibility", "visible");
-                }
+url = window.location.toString();
+console.log(url)
+if (url.includes("www.amazon.com") && (url.includes("dp") || url.includes("gp"))) {
+    port.postMessage({message: "put retailer", retailer: "Amazon"})
+}
 
-                // Make it hidden if it is visible
-                else {
-                    iframe_wrapper.style.setProperty("visibility", "hidden");
-                }
-            }
+port.onMessage.addListener(function(message) {
+    if (message.message == "get info") {
+        console.log('hereee')
+        var check = false;
+        let topics = document.getElementsByClassName("a-link-normal a-color-tertiary");
+        let category = document.getElementsByClassName("nav-search-label")[0].textContent;
+
+        if (category == "Electronics" || category == "Computers" || category == "All") {
+            console.log("herwewkljklwejrkljwklsjdlfjdsklfjklsajklfdjsklfj")
+            check = true;
         }
 
-        // Checks if the background_script sent a message to Check Electronics
-        if(request.status === "Check Electronics") {
-            let check = false;
-            let topics = document.getElementsByClassName("a-link-normal a-color-tertiary");
-            let category = document.getElementsByClassName("nav-search-label")[0].textContent;
-
-            console.log(category);
-            if (category == "Electronics" || "Computers") {
-                check = true;
+        if(check) {
+            let price;
+            try {
+                // Gets the price
+                price = document.getElementById("priceblock_ourprice").textContent;
             }
 
-            if (!check) {
-                for (let i = 0; i < topics.length; i++) {
-                    console.log(topics[i].textContent.replace(/\s/g, ""));
-                    if (topics[i].textContent.replace(/\s/g, "") == "Electronics") {
-                        check = true;
-                    }
-                }
-            }
-
-            if(check) {
-                let price;
+            catch(err) {
                 try {
-                    // Gets the price
-                    price = document.getElementById("priceblock_ourprice").textContent;
+                    // In case the priceblock_ourprice doesn't contian the price, get the dealprice instead (backup)
+                    price = document.getElementById("priceblock_dealprice").textContent;
                 }
 
-                catch(err) {
+                catch (err) {
                     try {
-                        // In case the priceblock_ourprice doesn't contian the price, get the dealprice instead (backup)
-                        price = document.getElementById("priceblock_dealprice").textContent;
+                        price = document.getElementById("priceblock_saleprice").textContent;
                     }
 
-                    catch (err) {
+                    catch(err) {
                         try {
-                            price = document.getElementById("priceblock_saleprice").textContent;
+                            price = document.getElementById("availability").textContent;
                         }
 
                         catch(err) {
-                            try {
-                                price = document.getElementById("availability").textContent;
-                            }
-
-                            catch(err) {
-                                price = "Price Not Available";
-                            }
+                            price = "Price Not Available";
                         }
                     }
                 }
-                // Defines both the item_model and the rows containing the item model (product description)
-                let item_model;
-                let row_content = document.getElementsByClassName("a-size-base");
-
-                // Gets the item_model through this loop
-                for(let i = 0; i < row_content.length; i++) {
-                    if(row_content[i].textContent.includes("Item model number")) {
-                        item_model = row_content[i+1].textContent;
-                    }
-                }
-                // Gets rid of the whitespace of the item_model
-                item_model = item_model.replace(/\s/g, "");
-
-                // Now, we have to get the title of the product
-                let productTitle = document.getElementById("productTitle").textContent.trim(" ");
-
-                // Sets the internal check to "Display"
-                internal_check = "Display";
-                // If it is, send message back to background_script to Get Data
-                console.log(productTitle);
-                sendResponse({status: "Get data", price: price, item_model: item_model, title: productTitle});
             }
+            // Defines both the item_model and the rows containing the item model (product description)
+            let item_model;
+            let row_content = document.getElementsByClassName("a-size-base");
 
-            else {
-                sendResponse({status: "Not valid"});
-            }
-        }
-
-        // Only display the iframe if the status is to display and the internal_check says to display
-        else if(request.status === "Display" && internal_check === "Display") {
-
-            // Allows you to use jquery
-            $(document).ready(function() {
-
-                // This is to remove the "old" iframe and replace it with the new one generated
-                if(internal_display_count >= 1) {
-                    document.getElementById("iframe-wrapper").remove();
+            // Gets the item_model through this loop
+            for(let i = 0; i < row_content.length; i++) {
+                if(row_content[i].textContent.includes("Item model number")) {
+                    item_model = row_content[i+1].textContent;
                 }
+            }
+            // Gets rid of the whitespace of the item_model
+            item_model = item_model.replace(/\s/g, "");
 
-                // Parse the request data that contains the html that has the iframe
-                request.data = JSON5.parse(request.data);
+            // Now, we have to get the title of the product
+            let productTitle = document.getElementById("productTitle").textContent.trim(" ");
 
-                // Put iframe where the left column is
-                document.getElementById("image-canvas-caption").innerHTML += request.data["iframe"];
-                // document.getElementsByTagName("body")[0].innerHTML += request.data["iframe"];
+            // If it is, send message back to background_script to Get Data
+            console.log(productTitle);
 
-                // Insert the html from the server in necessary places
-                $("iframe").contents().find("head").html(request.data["head"]);
-                $("iframe").contents().find("body").html(request.data["body"]);
-
-                // If the "X" is clicked, make it hidden
-                $("iframe").contents().find("#close-button").click(function() {
-                    iframe_wrapper.style.setProperty("visibility", "hidden");
-                });
-                iframe_wrapper = document.getElementById("iframe-wrapper");
-
-                // Scrolls up to the iframe that is in the leftCol
-                var top_of_iframe = $("#iframe-wrapper").position().top;
-                window.scrollTo({top: top_of_iframe - (top_of_iframe * 0.1), left: 0, behavior: "smooth"});
-
-                // For when the url changes on the same amazon page; Makes sure there is only 1 gui
-                internal_display_count += 1;
-            });
-            // VERY IMPORTANT: Sets the internal_check to "DO NOTHING" so that the if statement will fail
-            internal_check = "DO NOTHING";
+            internal_check += 0;
+             // Send message to send request to timeless apps
+            port.postMessage({message: "send request", item_model: item_model, price: price, title: productTitle});
         }
     }
-);
+
+
+    if (message.message == "add gui") {
+
+        // Allows you to use jquery
+        $(document).ready(function() {
+
+            // This is to remove the "old" iframe and replace it with the new one generated
+            if(internal_display_count >= 1) {
+                document.getElementById("iframe-wrapper").remove();
+            }
+
+            // Put iframe where the left column is
+            document.getElementById("image-canvas-caption").innerHTML += message.iframe;
+
+            // Insert the html from the server in necessary places
+            console.log(message);
+            $("iframe").contents().find("head").html(message.head);
+            $("iframe").contents().find("body").html(message.body);
+
+            // If the "X" is clicked, make it hidden
+            $("iframe").contents().find("#close-button").click(function() {
+                iframe_wrapper.style.setProperty("visibility", "hidden");
+            });
+
+            iframe_wrapper = document.getElementById("iframe-wrapper");
+
+            // Scrolls up to the iframe that is in the leftCol
+            var top_of_iframe = $("#iframe-wrapper").position().top;
+            window.scrollTo({top: top_of_iframe - (top_of_iframe * 0.1), left: 0, behavior: "smooth"});
+
+            // For when the url changes on the same amazon page; Makes sure there is only 1 gui
+            internal_display_count += 1;
+        });
+        // VERY IMPORTANT: Sets the internal_check to "DO NOTHING" so that the if statement will fail
+        internal_check = "DO NOTHING";
+    }
+});
