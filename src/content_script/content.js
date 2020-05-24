876c4d499c83601f347833955061413bf6896c5d
Jason Acheampong <Ultimi450@gmail.com>
2020-05-24 18:21:41 -0400

    created dictionary object if we want to use different retailers

--- src/content_script/content.js
+++ src/content_script/content.js
@@ -2,6 +2,19 @@ import Vue from 'vue';
 import Content from './ContentApp';
 import { amazon } from './Amazon';
 
+const retailerDict = {
+  Amazon: amazon,
+};
+
+let retailerName;
+const url = window.location.toString();
+if (url.includes('www.amazon.com')) {
+  retailerName = 'Amazon';
+}
+
+const retailer = retailerDict[retailerName];
+retailer.extractAllInfo();
+
 const div = document.createElement('div');
 div.id = 'price-assist';
 document.getElementsByTagName('body')[0].appendChild(div);
@@ -13,5 +26,3 @@ new Vue({
     return h(Content);
   },
 });
-
-amazon.extractAllInfo();
