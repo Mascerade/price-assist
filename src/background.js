3b02abcbd972de218305e4b3a4efed2ac4dacc01
Jason Acheampong <Ultimi450@gmail.com>
2020-06-01 07:52:46 -0400

    we now get the popup saying to sign in

--- src/background.js
+++ src/background.js
@@ -85,5 +85,18 @@ function connected(p) {
 }
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log('here');
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      const token = result.credential.accessToken;
+      const user = result.user;
+    })
+    .catch(error => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.email;
+      const credential = error.credential;
+    });
 });
