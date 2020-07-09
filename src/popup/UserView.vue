52c3c1b7445e6b21d418706ebbccba88688ef3f8
Jason Acheampong <Ultimi450@gmail.com>
2020-07-09 11:55:29 -0400

    finished the design for the popup

--- src/popup/UserView.vue
+++ src/popup/UserView.vue
@@ -6,8 +6,9 @@
         <h4 id="name">Jason Acheampong</h4>
       </div>
     </div>
-    <div id="content">
-      <div id="saved-products">
+
+    <div v-if="!productView" class="content">
+      <div @click="productView = true" id="saved-products">
         <span class="material-icons">check_circle_outline</span>
         <div id="saved-products-text-container">
           <p>{{ savedProducts.length }} products currently <span>saved!</span></p>
@@ -18,6 +19,22 @@
         <a href="http://www.timeless-apps.com/" target="_blank">Check Out Track Prices!</a>
       </div>
     </div>
+
+    <div v-if="productView" class="content">
+      <div id="products-container">
+        <div id="product-list-item" v-for="product in savedProducts" :key="product.itemModel">
+          <p>{{ product.title | truncate }}</p>
+          <div style="display: flex; flex: 1; flex-flow: row; justify-content: flex-end;">
+            <a @click.prevent="removeProduct(product.itemModel)">Remove</a>
+          </div>
+        </div>
+      </div>
+      <div @click="productView = false" id="back">
+        <span class="material-icons">chevron_left</span>
+        <p>Back</p>
+      </div>
+    </div>
+
     <a id="sign-out" @click.prevent="signOut()">Sign Out</a>
   </div>
 </template>
@@ -27,19 +44,32 @@ export default {
   data () {
     return {
       profileImg: 'https://lh3.googleusercontent.com/ogw/ADGmqu9CohlcoBgcFIjT__icy6vLfyusMxjm0m05nJhd=s32-c-mo',
+      productView: false,
       savedProducts: [
-        { 'item-model': 'item-model', title: 'Title' },
-        { 'item-model': 'item-model', title: 'Title' },
-        { 'item-model': 'item-model', title: 'Title' },
-        { 'item-model': 'item-model', title: 'Title' },
-        { 'item-model': 'item-model', title: 'Title' },
-        { 'item-model': 'item-model', title: 'Title' }
+        { itemModel: 'item-model', title: 'Intel Core i9-9900K Desktop Processor 8 Cores up to 5.0 GHz Turbo unlocked LGA1151 300 Series 95W' },
+        { itemModel: 'item-model', title: 'Intel NUC 10 Performance Kit – Intel Core i7 Processor (Tall Chassis)' },
+        { itemModel: 'item-model', title: 'Intel Core i7-10700 Desktop Processor 8 Cores up to 4.8 GHz LGA 1200 (Intel 400 Series Chipset) 65W, BX8070110700' },
+        { itemModel: 'item-model', title: 'Intel Core i7-9700K Desktop Processor 8 Cores up to 4.9 GHz Turbo unlocked LGA1151 300 Series 95W' },
+        { itemModel: 'item-model', title: 'AMD Ryzen 5 3600 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Stealth Cooler' },
+        { itemModel: 'item-model', title: 'AMD Ryzen 5 3600X 6-Core, 12-Thread Unlocked Desktop Processor with Wraith Spire Cooler' }
       ]
     }
   },
   methods: {
     signOut () {
       console.log('have to sign out')
+    },
+    removeProduct (itemModel) {
+      console.log('gotta remove ' + itemModel)
+    }
+  },
+  filters: {
+    truncate (value) {
+      if (value.length > 25) {
+        return value.slice(0, 25) + '...'
+      } else {
+        return value
+      }
     }
   }
 }
@@ -83,10 +113,50 @@ export default {
   justify-content: center;
 }
 
-#content {
+.content {
   display: flex;
   flex-flow: column;
   flex: 1;
+}
+
+#product-list-item {
+  display: flex;
+  flex-flow: row;
+  align-items: center;
+  padding-left: 5px;
+  padding-right: 5px;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  margin-top: 3px;
+  margin-bottom: 3px;
+  background-color: #fcfcfc;
+}
+
+#product-list-item > p {
+  margin: 0;
+}
+
+#product-list-item > div > a {
+  color: #40c4ff;
+}
+
+#product-list-item > div > a:hover {
+  cursor: pointer;
+}
+
+#back {
+  cursor: pointer;
+  display: flex;
+  flex-flow: row;
+  width: 100%;
+  justify-content: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  color: #40c4ff;
+}
+
+#back > p {
+  margin: 0;
 }
 
 #saved-products {
@@ -96,6 +166,10 @@ export default {
   height: 50px;
   margin-top: 8px;
   background-color: #fcfcfc;
+}
+
+#saved-products:hover {
+  cursor: pointer;
 }
 
 #saved-products > span {
